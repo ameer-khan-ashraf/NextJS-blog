e5ca107a92e0724024e42650ccc2b61253852b9b
Ameer <lovme.amir@gmail.com>
2022-05-05 14:54:11 +0400

    first commit

---
+++ components/Categories.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { getCategories } from '../services'
+
+const Categories = () => {
+  // const [categories, setCategories] = useState<any[]>([]);
+
+  // useEffect(()=>{
+  //   getCategories()
+  //   .then((newCategories:any)=> {
+  //     setCategories(newCategories)
+  //   })
+  // },[]);
+
+  let categories = [
+    {
+      "name": "Web Development",
+      "slug": "webdev"
+    }
+  ]
+
+  const categoryList = categories.map((category:{name: string; slug: string})=>{
+    <Link key={category.slug} href={`/category/${category.slug}`} >
+      <span className='cursor-pointer block pb-3 mb-3'>
+        {category.name}
+      </span>
+    </Link>
+  })
+  
+  return (
+    <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+        Categories
+      </h3>
+      {categoryList}
+    </div>
+  )
+}
+
+export default Categories
---
+++ pages/index.tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services'
+
+
+const Home: NextPage = ({posts}:any) => {
+  return (
+    <div className="container mx-auto px-10 mb-8 ">
+      <Head>
+        <title>NextJS Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post:any)=>{
+            return(
+              <PostCard post={post.node} key ={post.node.title}/>
+            )
+          })}
+        </div>
+        <div className='lg:col-span-4 col-span-1'>
+          <div className='lg:sticky relative top-8'>
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
+
+export async function getStaticProps(){
+  const posts : any = (await getPosts())||[];
+
+  return{
+    props:{ posts }
+  }
+}
